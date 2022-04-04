afcbbc6c61b4406d9684892eb38f88ff521267c7
Guilherme <acousg1@gmail.com>
2022-04-04 20:38:39 -0300

    att

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-alert('Clicar 3 vezes em menos de 5 segundos')
-
 let contador = 0
 let ss01 = 4
 let ss02 = 0
@@ -8,67 +6,76 @@ let cronometro01
 let cronometro02
 
 function start(){
+    let começar = document.getElementById('começar').style = 'display: none;'
     let tempo = 1000
-    clearInterval(cronometro01)//caso clicar mais de uma vez, n vai acelerar o 'tempo'
-    cronometro01 = setInterval(timer1, tempo)//timer1 é o timer regressivo 
+    cronometro01 = setInterval(timer1, tempo)//timer1 é o timer regressivo
+    
 
 }//começa o cronometro
-
-
 
 function timer1(){
     ss01--
     
-    let formato01 = `O jogo começará em ${ss01}`
+    let formato01 = `O jogo começará em: <br> <strong>${ss01}</strong>`
     let timer01 = document.getElementById('timer-01').innerHTML = formato01//texto que vai aparecer quando clicar em 'Começar'
 
     if(ss01 === 0){//aqui o segundos zera e começa o jogo
         clearInterval(cronometro01)
-        ss01 = 4
         let timer01 = document.getElementById('timer-01').innerHTML = ''//aqui vai sumir o paragrafo do timer regressivo
 
-
-        let resposta = document.getElementById('resposta')
-        resposta.innerHTML = '<button class="clique" onclick="clicar()">Clique aqui</button>'//aparece o botao pra pessoa começar a clicar
-
         let tempo = 1000
-        cronometro02 = setInterval(timer2, tempo)
+        cronometro02 = setInterval(timer2, tempo)//começa o segundo timer
     }
-}
 
-function timer2(){
+    function timer2(){
         ss02++
-        let formato02 = `${ss02}`
-        let timer02 = document.getElementById('timer-02').innerHTML = formato02
+        
+        let formato02 = `cronômetro: ${ss02}`
+        let timer02 = document.getElementById('timer-02').innerHTML = formato02//timer2
+        let numCliques = document.getElementById('numCliques')
+        let resposta = document.getElementById('resposta')
 
-        if(ss02 < 5 && contador === 3){
-            let ganhou = document.getElementById('ganhou')
-            ganhou.innerHTML = 'Parabéns você ganhou :)'
+        resposta.innerHTML = '<button onclick="clicar()" id="contador"></button>'
+                                +'<button onclick="pararTudo()" id="a">Parar</button>'//aparece o botao pra pessoa começar a clicar
+        numCliques.innerHTML = `Você clicou <strong>${contador}</strong> vezes`
+
+        if(contador >= 5 && ss02 <= 5){
+            let resposta = document.getElementById('resposta').style = 'display: none;'//botao do contador some
+            let numCliques = document.getElementById('numCliques').innerHTML = ''
+            let timer02 = document.getElementById('timer-02').innerHTML = ''
+            let recomeçar = document.getElementById('recomeçar')
+            recomeçar.innerHTML = '<button onclick="recomeçar()" id="recomeçar02">Recomeçar</button>'
 
             clearInterval(cronometro02)
-            contador = 0
-            ss = 0 
-            let timer02 = document.getElementById('timer-02').innerHTML = ''
-            let numCliques = document.getElementById('numCliques').innerHTML = ''
+
+            let ganhou = document.getElementById('ganhou').innerHTML = 'Parabéns você ganhou :)' 
         }
-        else if(ss02 === 5 && contador < 3){
-            let perdeu = document.getElementById('perdeu')
-            perdeu.innerHTML = 'Você perdeu :('
+        else if(contador <= 4 && ss02 >= 5){
+            let resposta = document.getElementById('resposta').style = 'display: none;'//botao do contador some
+            let numCliques = document.getElementById('numCliques').innerHTML = ''
+            let timer02 = document.getElementById('timer-02').innerHTML = ''
+            let recomeçar = document.getElementById('recomeçar')
+            recomeçar.innerHTML = '<button onclick="recomeçar()" id="recomeçar02">Recomeçar</button>'    
 
             clearInterval(cronometro02)
-            contador = 0
-            ss = 0 
-            let timer02 = document.getElementById('timer-02').innerHTML = ''
-            let numCliques = document.getElementById('numCliques').innerHTML = ''
+
+            let perdeu = document.getElementById('perdeu').innerHTML = 'Você perdeu :('
         }
-}//segundo timer
+    }//timer2
+}//timer1
 
 function clicar(){
-    let numCliques = document.getElementById('numCliques')
     contador++
 
-    numCliques.innerHTML = `Você clicou ${contador} vezes`
+    let numCliques = document.getElementById('numCliques').innerHTML = `Você clicou <strong>${contador}</strong> vezes`
 }//clicou tantas vezes
 
 
-    
+function recomeçar(){
+    location.reload()
+}//atualiza a pagina
+
+
+
+
+
